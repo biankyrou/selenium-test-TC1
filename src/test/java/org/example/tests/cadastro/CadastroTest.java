e4ad63ce480f3b77cd0b82ada579cf480185cb0b
biankyrou <bianca.carvalio@aluno.ifsp.edu.br>
2024-11-27 15:25:03 -0300

    feat: new functional tests to test element visibility

--- src/test/java/org/example/tests/cadastro/CadastroTest.java
+++ src/test/java/org/example/tests/cadastro/CadastroTest.java
@@ -29,13 +29,47 @@ public class CadastroTest {
         driver.quit();
     }
 
+
+    // testes funcionais:
     @Test
     @DisplayName("Verifica se o campo \"Nome\" está visível")
     public void testNomeInputVisivel() {
         WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
         wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[@id='nome']")));
-
         Assertions.assertTrue(cadastroPage.isNomeInputVisible(), "O campo Nome não está visível!");
     }
 
+    @Test
+    @DisplayName("Verifica se o campo \"Idade\" está visível")
+    public void testIdadeInputVisivel() {
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[@id='idade']")));
+        Assertions.assertTrue(cadastroPage.isIdadeInputVisible(), "O campo Idade não está visível!");
+    }
+
+    @Test
+    @DisplayName("Verifica se o Cadastrar Button está visível")
+    public void testCadastrarButtonVisivel() {
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[@id=\"cadastroForm\"]/button")));
+        Assertions.assertTrue(cadastroPage.isCadastrarButtonVisible(), "O botão Cadastrar não está visível!");
+    }
+
+    @Test
+    @DisplayName("Verifica se o Visualizar Lista Button está visível")
+    public void testVisualizarButtonVisivel() {
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("/html/body/div/a")));
+        Assertions.assertTrue(cadastroPage.isVisualizarButtonVisible(), "O botão Visualizar não está visível!");
+    }
+
+
+    @Test
+    @DisplayName("Verifica se os campos de input e botões estão visíveis")
+    public void testCadastroPageCarregada() {
+        Assertions.assertTrue(cadastroPage.isNomeInputVisible());
+        Assertions.assertTrue(cadastroPage.isIdadeInputVisible());
+        Assertions.assertTrue(cadastroPage.isCadastrarButtonVisible());
+        Assertions.assertTrue(cadastroPage.isVisualizarButtonVisible());
+    }
 }
