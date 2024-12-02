9f7516010a1022390296f4e14eb16fcbcfd0b4da
Joao Pedro Volante <joaopedrovolante@gmail.com>
2024-12-02 20:15:45 -0300

    fix: ListaSuiteTest Class

--- src/test/java/org/example/tests/lista/ListaSuiteTest.java
+++ src/test/java/org/example/tests/lista/ListaSuiteTest.java
@@ -28,48 +28,28 @@ import java.time.Duration;
 })
 public class ListaSuiteTest {
 
-    private WebDriver driver;
-    private CadastroPage cadastroPage;
-    private final Faker faker = new Faker();
 
-    @BeforeEach
-    public void setUp() {
-        WebDriverManager.chromedriver().setup();
-        driver = new ChromeDriver();
-        cadastroPage = PageFactory.initElements(driver, CadastroPage.class);
-        driver.get("https://tc-1-final-parte1.vercel.app/tabelaCadastro.html");
-    }
-
-    @AfterEach
-    public void tearDown() {
-        if (driver != null) {
-            driver.quit();
-        }
-    }
-
-
-
-    @Test
-    @DisplayName("Exibir botão 'Editar' para cada pessoa na lista")
-    public void testExibirBotaoEditar() {
-        JavascriptExecutor js = (JavascriptExecutor) driver;
-        js.executeScript("localStorage.setItem('pessoas', JSON.stringify([{ nome: 'Marcio', idade: 25 }, { nome: 'Mariana', idade: 30 }]))");
-        driver.navigate().refresh();
-
-        List<WebElement> editarButtons = driver.findElements(By.xpath("//button[text()='Editar']"));
-        assertEquals(2, editarButtons.size(), "Deve existir um botão 'Editar' para cada pessoa na lista.");
-    }
-
-    @Test
-    @DisplayName("Exibir botão 'Remover' para cada pessoa na lista")
-    public void testExibirBotaoRemover() {
-        JavascriptExecutor js = (JavascriptExecutor) driver;
-        js.executeScript("localStorage.setItem('pessoas', JSON.stringify([{ nome: 'João', idade: 25 }, { nome: 'Maria', idade: 30 }]))");
-        driver.navigate().refresh();
-
-        List<WebElement> removerButtons = driver.findElements(By.xpath("//button[text()='Remover']"));
-        assertEquals(2, removerButtons.size(), "Deve existir um botão 'Remover' para cada pessoa na lista.");
-    }
+//    @Test
+//    @DisplayName("Exibir botão 'Editar' para cada pessoa na lista")
+//    public void testExibirBotaoEditar() {
+//        JavascriptExecutor js = (JavascriptExecutor) driver;
+//        js.executeScript("localStorage.setItem('pessoas', JSON.stringify([{ nome: 'Marcio', idade: 25 }, { nome: 'Mariana', idade: 30 }]))");
+//        driver.navigate().refresh();
+//
+//        List<WebElement> editarButtons = driver.findElements(By.xpath("//button[text()='Editar']"));
+//        assertEquals(2, editarButtons.size(), "Deve existir um botão 'Editar' para cada pessoa na lista.");
+//    }
+//
+//    @Test
+//    @DisplayName("Exibir botão 'Remover' para cada pessoa na lista")
+//    public void testExibirBotaoRemover() {
+//        JavascriptExecutor js = (JavascriptExecutor) driver;
+//        js.executeScript("localStorage.setItem('pessoas', JSON.stringify([{ nome: 'João', idade: 25 }, { nome: 'Maria', idade: 30 }]))");
+//        driver.navigate().refresh();
+//
+//        List<WebElement> removerButtons = driver.findElements(By.xpath("//button[text()='Remover']"));
+//        assertEquals(2, removerButtons.size(), "Deve existir um botão 'Remover' para cada pessoa na lista.");
+//    }
 
 
 
